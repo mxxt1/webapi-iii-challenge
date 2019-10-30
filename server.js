d69c4fd1215fe0dc3ab9c1e9606b7cee9ee0da1e
mxxt1 <matt@dustcloudlabs.com>
2019-10-30 18:46:49 -0400

    get users and post new user working. Having technical issues with windows

--- server.js
+++ server.js
@@ -20,16 +20,18 @@ function logger(req, res, next) {
 
 server.use(logger);
 server.use(express.json());
-server.use(validateUser);
-server.use(validateUserId);
-server.use(validatePost);
+// server.use(validateUser);
+// server.use(validateUserId);
+// server.use(validatePost);
+
+
+// server.get('/', (req, res) => {
+//   res.send(`<h2>Let's write some middleware!</h2>`)
+// });
+
+server.use('/',userRouter);
 
 server.use('/api/users', userRouter);
-
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
-});
-
 
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -4,15 +4,33 @@ const db = require('./userDb.js');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-
+    db.insert(req.body)
+    .then(response => {
+        res.status(400).json(response)
+    })
+    .catch(err => {
+        res.status(500).json({Error: `failed to write to database`})
+    })
 });
 
 router.post('/:id/posts', (req, res) => {
-
+    db.insert(req.body)
+    .then(response => {
+        res.status(400).json(response)
+    })
+    .catch(err => {
+        res.status(500).json({Error: `Failed to add post to user id ${req.params.id}`})
+    })
 });
 
 router.get('/', (req, res) => {
-
+    db.get()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        res.status(500).json({message: `Failed to retrieve users`})
+    })
 });
 
 router.get('/:id', (req, res) => {
@@ -34,6 +52,8 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
+    // if (req.params.id)
+
 
 };
 
