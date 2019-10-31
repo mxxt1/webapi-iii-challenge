9628ba581a382db34c6bfe7c1c5891782421a5c9
mxxt1 <matt@dustcloudlabs.com>
2019-10-31 10:59:29 -0400

    middleware functions built

--- users/userRouter.js
+++ users/userRouter.js
@@ -33,7 +33,7 @@ router.get('/', (req, res) => {
     })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, validateUserId, res) => {
 
 });
 
@@ -52,17 +52,35 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-    // if (req.params.id)
-
-
+    db.getById(req.params.id)
+    .then(user => {
+        if (user === undefined || user === null || user === {}){
+            res.status(400).json({message: `User not found or authorized`})
+        } else {
+            req.user = user;
+            next();
+        }
+    })
 };
 
 function validateUser(req, res, next) {
-
+    if (!req.body){
+        res.status(400).json({message: `Please include all user data`})
+    } else if (!req.body.name){
+        res.status(400).json({message: `Missing 'name' data`})
+    } else {
+        next();
+    }
 };
 
 function validatePost(req, res, next) {
-
+    if (!req.body){
+        res.status(400).json({message: `Post data does not exist`})
+    } else if (!req.body.text){
+        res.status(400).json({message: `Please include all data`})
+    } else {
+        next();
+    }
 };
 
 module.exports = router, validateUserId, validateUser, validatePost;
